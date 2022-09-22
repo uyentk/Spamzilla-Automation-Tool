2b1c366c50df2fe96ed797804a4d8b1b7246d593
Winnie Tran <jennieathanasiatran@gmail.com>
2022-09-22 10:56:02 +0700

    Fix index.py

--- data_analyse.py
+++ data_analyse.py
@@ -3,21 +3,22 @@ from datetime import datetime, timedelta
 import os
 
 # Rename exported file
-for (root, dirs, files) in os.walk('D:\ASM\SpamzillaExport\Spamzilla-Automation-Tool\data', topdown=True):
-    print (files)
-os.rename(f"D:\ASM\SpamzillaExport\Spamzilla-Automation-Tool\data/{files[0]}", "D:\ASM\SpamzillaExport\Spamzilla-Automation-Tool\data\exported_data.csv")
+# for (root, dirs, files) in os.walk('D:\ASM\SpamzillaExport\Spamzilla-Automation-Tool\data', topdown=True):
+#     print (files)
+# os.rename(f"D:\ASM\SpamzillaExport\Spamzilla-Automation-Tool\data/{files[0]}", "D:\ASM\SpamzillaExport\Spamzilla-Automation-Tool\data\exported_data.csv")
 
 def data_analyse(exporeted_file):
+    for (root, dirs, files) in os.walk('D:\ASM\SpamzillaExport\Spamzilla-Automation-Tool\data', topdown=True):
+        print (files)
+    os.rename(f"D:\ASM\SpamzillaExport\Spamzilla-Automation-Tool\data/{files[0]}", "D:\ASM\SpamzillaExport\Spamzilla-Automation-Tool\data\exported_data.csv")
     tomorrow = datetime.now() + timedelta(days = 1)
-    dt_string = tomorrow.strftime("%d/%m/%Y") 
+    dt_string = tomorrow.strftime("%m/%d/%Y") 
+    print(dt_string)
     df = pd.read_csv(exporeted_file)
-    if len(df) == 0:
-        print("There's no data")
-    else:
-        df.sort_values(by= "Expires", inplace= True)
-        df = df[df["Expires"].str.match(dt_string) == True]
-        res = df[["Name", "Source", "TF", "CF", "Age", "Price", "Expires"]]
-        res.to_csv("Spamzilla-Automation-Tool\output.csv")
+    df.sort_values(by= "Expires", inplace= True)
+    df = df[df["Expires"].str.match(dt_string) == True]
+    res = df[["Name", "Source", "TF", "CF", "Age", "Price", "Expires"]]
+    res.to_csv("Spamzilla-Automation-Tool\output.csv")
 
 # paste the path of exported file here
-# data_analyse("D:\ASM\SpamzillaExport\Spamzilla-Automation-Tool\data\export-61508_2022-09-20-09-50-47.csv")
+# data_analyse("Spamzilla-Automation-Tool\data\exported_data.csv")
--- index.py
+++ index.py
@@ -1,8 +1,9 @@
-from data_analyse import data_analyse
 from automation import App
+from data_analyse import data_analyse
 
 def Spamzilla_Tool():
-    data_analyse()
-    App("D:\ASM\SpamzillaExport\Spamzilla-Automation-Tool\data\exported_data.csv")
+    App()
+    print("Start to data analysing")
+    data_analyse("Spamzilla-Automation-Tool\data\exported_data.csv")
 
 Spamzilla_Tool()
